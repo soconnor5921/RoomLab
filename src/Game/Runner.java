6962146ef705b88ef90a1348eb0cb6766176fc72
BT_1N3_32 <soconnor5921@bths.edu>
2018-10-30 09:10:52 -0400

    10/30

--- src/Game/Runner.java
+++ src/Game/Runner.java
@@ -1,6 +1,7 @@
 package Game;
 
 import People.Person;
+import Rooms.HauntedRoom;
 import Rooms.Room;
 import Rooms.WinningRoom;
 
@@ -28,6 +29,11 @@ public class Runner {
 		int x = (int)(Math.random()*building.length);
 		int y = (int)(Math.random()*building.length);
 		building[x][y] = new WinningRoom(x, y);
+
+		//Create a random haunted room.
+		int a = (int)(Math.random()*building.length);
+		int b = (int)(Math.random()*building.length);
+		building[a][b] = new HauntedRoom(a, b);
 		 
 		 //Setup player 1 and the input scanner
 		Person player1 = new Person("FirstName", "FamilyName", 0,0);
---
+++ src/Rooms/HauntedRoom.java
@@ -0,0 +1,25 @@
+package Rooms;
+
+import Game.Runner;
+import People.Person;
+
+public class HauntedRoom extends Room
+{
+    public HauntedRoom(int x, int y)
+    {
+        super(x, y);
+    }
+
+    public void enterRoom(Person x)
+    {
+        occupant = x;
+        x.setxLoc(this.xLoc);
+        x.setyLoc(this.yLoc);
+        System.out.println("You found the haunted room, get out fast before the ghosts get you!");
+    }
+
+    public void leaveRoom(Person x)
+    {
+        occupant = null;
+    }
+}
